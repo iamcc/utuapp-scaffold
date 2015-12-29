38df042985a6d778ad2bacc001a7afc1ed3120d0
cc <sic.hcq@gmail.com>
2015-12-29 15:40:26 +0800

    update config files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: CC
 * @Date:   2015-12-29 12:25:10
 * @Last Modified by:   CC
-* @Last Modified time: 2015-12-29 13:59:04
+* @Last Modified time: 2015-12-29 15:34:47
 */
 
 'use strict';
@@ -11,7 +11,7 @@ var webpack = require('webpack');
 var __DEV__ = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: ['src/index.js'],
+  entry: ['./src/index.js'],
   output: {
     filename: 'bundle.js',
     path: 'dist',
@@ -22,12 +22,12 @@ module.exports = {
       {
         test: /\.js$/,
         loader: (__DEV__ ? 'react-hot!' : '') + 'babel',
-        include: 'src',
+        exclude: /node_modules/,
       },
       { test: /\.css$/, loader: 'style!css' },
-      { test: /\.(png|jpg|jpeg|gif|svg)/, loader: 'url-loader?limit=10000' },
-      { test: /\.(woff2?)/, loader: 'url-loader?limit=10000' },
-      { test: /\.(ttf|eot|wav|mp3)/, loader: 'file-loader' },
+      { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader?limit=10000' },
+      { test: /\.(woff2?)$/, loader: 'url-loader?limit=10000' },
+      { test: /\.(ttf|eot|wav|mp3)$/, loader: 'file-loader' },
       { test: /\.json$/, loader: 'json' },
       { test: /\.html$/, loader: 'html' },
       { test: /\.md$/, loader: 'html|markdown' },
@@ -38,5 +38,4 @@ module.exports = {
 
 if (__DEV__) {
   module.exports.devtool = 'inline-source-map';
-  module.exports.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
